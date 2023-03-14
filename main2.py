3c8a5fc0946cde1ea53eca5b4a7d10296ef593ff
rlm42 <56745971+rlm42@users.noreply.github.com>
2023-03-14 16:51:48 +0400

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,13 @@
+import reddit_image_extractor_module2
+import time as _time
+
+def main():
+    reddit_image_extractor_module2.run_reddit_downloader()
+
+def run():
+    while True:
+        main()
+        _time.sleep(86400)  ## in seconds (this is 24 hours)
+
+if __name__ == "__main__":
+    run()

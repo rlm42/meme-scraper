86163b962e8f42bdd264d3443a5d4a61262cc944
rlm42 <56745971+rlm42@users.noreply.github.com>
2023-03-14 12:50:58 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# A list of (string) default subreddits you wish to download example: ["wallpapers" , "ultrahdwallpapers"]
+subreddit = ["cryptocurrencymemes"]
+# Set default Reddit sort example: "top", Empty will result in "hot" sort
+sort_type = ""
+# Add default arguments for the selected sort type example for top: "sort=top&t=all". Note not all sort types have arguments
+sort_arg = ""
+# Set the default for how many posts to download per subreddit
+down_limit = 1
---
+++ main.py
@@ -0,0 +1,7 @@
+import reddit_image_extractor_module2
+
+def main():
+    reddit_image_extractor_module2.run_reddit_downloader()
+
+if __name__ == "__main__":
+    main()

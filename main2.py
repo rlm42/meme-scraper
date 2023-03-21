13f382effe33bb3011d34224edb789722eb65f55
rlm42 <56745971+rlm42@users.noreply.github.com>
2023-03-21 22:09:58 +0400

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,33 @@
+import reddit_image_extractor_module2
+import time as _time
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+# def main():
+#     reddit_image_extractor_module2.run_reddit_downloader()
+
+
+# def run():
+#     while True:
+#         main()
+#         _time.sleep(86400)  ## in seconds (this is 24 hours)
+
+# main()
+# _time.sleep(86400)  # in seconds (this is 24 hours)
+
+
+# @app.route("/")
+# def main():
+#     while True:
+#         reddit_image_extractor_module2.run_reddit_downloader()
+#         _time.sleep(86400)  # in seconds (this is 24 hours)
+
+
+if __name__ == "__main__":
+    # run()
+    app.run(debug=True)
+    while True:
+        reddit_image_extractor_module2.run_reddit_downloader()
+        _time.sleep(86400)  # in seconds (this is 24 hours)
